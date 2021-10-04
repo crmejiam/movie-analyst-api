3fadd1b6f26a2845c55cdbb4c5198fe3d02277e7
Cristian Mejia <crmejiam120@gmail.com>
2021-10-04 11:53:38 -0500

    Adding nock to mock db requests and try to make /movies endpoint unit test

--- test/test.js
+++ test/test.js
@@ -5,14 +5,15 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let nock = require('nock');
+
+const endpoint = 'http://127.0.0.1:3000'
 
 chai.use(chaiHttp);
 //Our parent block
 describe('API', () => {
 
-/*
-  * Test the /GET route
-  */
+  // Test the /GET Route
   describe('/GET home', () => {
       it('it should GET any reply', (done) => {
         chai.request(server)
@@ -24,4 +25,20 @@ describe('API', () => {
       });
   });
 
+  // Test the /movies API Endpoint
+  describe('/GET movies', () => {
+    it('it should GET a JSON with data of the movies', (done) => {
+      let request = nock(endpoint)
+        .get('/movies')
+        .reply(200, movies = [{
+          "title":"Batman V Superman",
+          "release":"2016",
+          "score":6,
+          "reviewer":"Martin Thomas"
+        }])
+        request.should.have.status(200);
+        request.movies.should.be.an('array');
+        request.movies.should.have.keys('title', 'release', 'score', 'reviewer');
+    });
+  });
 });

126598aa72944846ba57b30905c7d0e41739c504
Cristian Mejia <crmejiam120@gmail.com>
2021-10-04 14:22:27 -0500

    Adding tests for the other endpoints

--- test/test.js
+++ test/test.js
@@ -4,8 +4,7 @@ process.env.NODE_ENV = 'test';
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
-let should = chai.should();
-let expect = chai.expect(); 
+let should = chai.should(); 
 let nock = require('nock');
 
 const API_IP = 'http://127.0.0.1:3000'
@@ -45,8 +44,80 @@ describe('API', () => {
           res.should.have.status(200);
           res.body.should.be.an('array');
           res.body[0].should.have.keys('title', 'release', 'score', 'reviewer');
+          res.body.should.have.lengthOf(1);
         done();
       });
     });
   });
+
+  // Test the /reviewers API Endpoint
+  describe('/GET reviewers', () => {
+    it('it should GET a JSON with data of the reviewers', (done) => {
+
+      mockAPI.get('/reviewers')
+      .reply(200, [{
+        "name":"Andrew West",
+        "avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg",
+        "publication":"MyNextReview"
+      }]);
+
+      chai.request(API_IP)
+      .get('/reviewers')
+      .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body[0].should.have.keys('name', 'avatar', 'publication');
+          res.body.should.have.lengthOf(1);
+        done();
+      });
+    });
+  });
+
+  // Test the /publications API Endpoint
+  describe('/GET publications', () => {
+    it('it should GET a JSON with data of the publications', (done) => {
+
+      mockAPI.get('/publications')
+      .reply(200, [{
+        "name":"ComicBookHero.com",
+        "avatar":"glyphicon-flash"
+      }]);
+
+      chai.request(API_IP)
+      .get('/publications')
+      .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body[0].should.have.keys('name', 'avatar');
+          res.body.should.have.lengthOf(1);
+        done();
+      });
+    });
+  });
+
+  // Test the /pending API Endpoint
+  describe('/GET pending', () => {
+    it('it should GET a JSON with data of the pending movie reviews', (done) => {
+
+      mockAPI.get('/pending')
+      .reply(200, [{
+        "title":"Doctor Strange",
+        "release":"2016",
+        "score":7,
+        "reviewer":"Anthony Miller",
+        "publication":"ComicBookHero.com"
+      }]);
+
+      chai.request(API_IP)
+      .get('/pending')
+      .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body[0].should.have.keys('title', 'release', 'score', 'reviewer', 'publication');
+          res.body.should.have.lengthOf(1);
+        done();
+      });
+    });
+  });
+
 });

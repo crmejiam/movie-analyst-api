d07e5a35c2ac08de79e573966b44a09a580395a7
Cristian Mejia <crmejiam120@gmail.com>
2021-10-04 12:11:08 -0500

    Trying to fix /movies endpoint unit test

--- test/test.js
+++ test/test.js
@@ -5,9 +5,11 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let expect = chai.expect(); 
 let nock = require('nock');
 
-const endpoint = 'http://127.0.0.1:3000'
+const API_IP = 'http://127.0.0.1:3000'
+const mockAPI = nock(API_IP);
 
 chai.use(chaiHttp);
 //Our parent block
@@ -28,17 +30,23 @@ describe('API', () => {
   // Test the /movies API Endpoint
   describe('/GET movies', () => {
     it('it should GET a JSON with data of the movies', (done) => {
-      let request = nock(endpoint)
-        .get('/movies')
-        .reply(200, movies = [{
-          "title":"Batman V Superman",
-          "release":"2016",
-          "score":6,
-          "reviewer":"Martin Thomas"
-        }])
-        request.should.have.status(200);
-        request.movies.should.be.an('array');
-        request.movies.should.have.keys('title', 'release', 'score', 'reviewer');
+
+      mockAPI.get('/movies')
+      .reply(200, [{
+        "title":"Batman V Superman",
+        "release":"2016",
+        "score":6,
+        "reviewer":"Martin Thomas"
+      }]);
+
+      chai.request(API_IP)
+      .get('/movies')
+      .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body[0].should.have.keys('title', 'release', 'score', 'reviewer');
+        done();
+      });
     });
   });
 });
